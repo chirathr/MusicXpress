6f2c4731907c11a094bc15b80ecef11943a30ab2
Chirath R <chirath.02@gmail.com>
2016-10-24 08:38:17 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-create table consumer(consumername varchar(20),consumerid varchar(10) primary key,password varchar(20));
+create table consumer(consumername varchar(20),consumerid numeric(10) primary key,password varchar(20));
 create table album(albumid varchar(10) primary key,name varchar(20).albumart bytea);
 create table songs(songid varchar(10) primary key,name varchar(20),artistname varchar(20), songart bytea,genre varchar(10),filepath varchar(25));
 create table rating(consumerid varchar(20),songid varchar(10) primary key,value integer, foreign key(consumerid) references consumer (consumerid));
