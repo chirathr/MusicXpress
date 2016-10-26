66194a26298bd9430e1e82d867afff1302398e80
Chirath02 <chirath.02@gmail.com>
2016-10-26 21:28:42 +0530

    created login

--- login.php
+++ login.php
@@ -3,6 +3,55 @@
  * Created by PhpStorm.
  * User: chirath
  * Date: 26/10/16
- * Time: 8:45 PM
+ * Time: 8:47 PM
  */
+
+$error = "";
+
+$connString = 'port=5432 dbname=musicexpress user=music password=password';
+
+$conn = pg_connect($connString);
+
+
+
+//$sql = "SELECT * FROM logins";
+//$result = pg_query($conn, $sql);
+
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    $username="";
+    $password="";
+}
+
+else if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username=trim($_POST["userNameLogin"]);
+    $password=trim($_POST["passwordLogin"]);
+
+//    if(pg_fetch_result($results, $userName, "userName")==true
+//        && pg_fetch_result($results, $password, "userName")==true) {
+//        setcookie("userIDforDV", $userName, time()+43200);
+//    }
+//    else {
+//        $error = "Your username and or password is incorrect";
+//    }
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = md5('$password');";
+    $result = pg_query($conn, $query);
+    if(pg_num_rows($result) != 1) {
+        // do error stuff
+        $error = "Your username and or password is incorrect";
+    } else {
+        // user logged in
+        setcookie("userIDforDV", $userName, time()+43200);
+    }
+
+}
+
+$userName = $_COOKIE['userIDforDV'];
+
+if(isset($userName) && $userName!="") {
+    echo "Welcome " . $userName;
+}
+
+echo $error;
+
 ?>
--- register.php
+++ register.php
@@ -8,27 +8,40 @@
 
 $error = "";
 
-$conn = pg_connect("host=localhost dbname=brittains_db user=brittains password=XXXX" );
+$connString = 'port=5432 dbname=musicexpress user=music password=password';
 
-$sql = "SELECT * FROM logins";
-$result = pg_query($conn, $sql);
+$conn = pg_connect($connString);
+
+
+
+//$sql = "SELECT * FROM logins";
+//$result = pg_query($conn, $sql);
 
 if($_SERVER["REQUEST_METHOD"] == "GET") {
-    $userName="";
+    $username="";
     $password="";
 }
 
 else if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $userName=trim($_POST["userNameLogin"]);
+    $username=trim($_POST["userNameLogin"]);
     $password=trim($_POST["passwordLogin"]);
 
-    if(pg_fetch_result($results, $userName, "userName")==true
-        && pg_fetch_result($results, $password, "userName")==true) {
-        setcookie("userIDforDV", $userName, time()+43200);
-    }
-    else {
+//    if(pg_fetch_result($results, $userName, "userName")==true
+//        && pg_fetch_result($results, $password, "userName")==true) {
+//        setcookie("userIDforDV", $userName, time()+43200);
+//    }
+//    else {
+//        $error = "Your username and or password is incorrect";
+//    }
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = md5('$password');";
+    $result = pg_query($conn, $query);
+    if(pg_num_rows($result) != 1) {
+        // do error stuff
         $error = "Your username and or password is incorrect";
+    } else {
+        // user logged in
+        setcookie("userIDforDV", $userName, time()+43200);
     }
 
 }
