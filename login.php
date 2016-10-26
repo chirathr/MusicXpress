7172b94cd11744c3df3dfd07a8b57fafabe47be2
Chirath02 <chirath.02@gmail.com>
2016-10-26 20:48:01 +0530

    started working on register.php

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chirath
+ * Date: 26/10/16
+ * Time: 8:45 PM
+ */
+?>
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chirath
+ * Date: 26/10/16
+ * Time: 8:47 PM
+ */
