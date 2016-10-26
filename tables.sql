460b95770bdaf7921dcffa7cd69e1ac991e9db6b
Chirath02 <chirath.02@gmail.com>
2016-10-26 20:12:35 +0530

    Edited tables.sql

--- tables.sql
+++ tables.sql
@@ -1,5 +1,35 @@
-create table consumer(consumername varchar(20),consumerid numeric(10) primary key,password varchar(20));
-create table album(albumid varchar(10) primary key,name varchar(20).albumart bytea);
-create table songs(songid varchar(10) primary key,name varchar(20),artistname varchar(20), songart bytea,genre varchar(10),filepath varchar(25));
-create table rating(consumerid varchar(20),songid varchar(10) primary key,value integer, foreign key(consumerid) references consumer (consumerid));
-create table playlist(songid varchar(10),albumid varchar(10),name varchar(20),foreign key(songid) references songs(songid),foreign key(albumid) references album(albumid));
+create table user(
+      id numeric(10) primary key,
+      username varchar(100) unique,
+      fullname varchar(100),
+      email varchar(100),
+      password varchar(32)
+    );
+
+create table album(
+      id numeric(10) primary key,
+      name varchar(100),
+      albumart varchar(100)
+    );
+
+create table song(
+      id numeric(10) primary key,
+      name varchar(100),
+      artistname varchar(100),
+      songart varchar(100),
+      genre varchar(100),
+      filepath varchar(100)
+    );
+
+create table rating(
+      songid numeric(10) references song(id),
+      userid numeric(10) references user(id),
+      value numeric(2)
+    );
+
+create table playlist(
+      songid numeric(10) references song(id),
+      albumid numeric(10) references album(id),
+      name varchar(1000),
+      song_order_numeber numeric(10)
+    );
