46485aaa18bb1edf8696e293825df75205841212
Chirath02 <chirath.02@gmail.com>
2016-10-30 13:58:54 +0530

    Removed unessary items

--- src/static-includes.php
+++ src/static-includes.php
@@ -9,9 +9,9 @@
 <link rel="stylesheet" href="css/bootstrap.min.css" >
 <!-- Optional theme -->
 <link rel="stylesheet" href="css/bootstrap-theme.min.css" >
-<!-- Latest compiled and minified JavaScript -->
-<script src="js/bootstrap.min.js" ></script>
 <!-- jQuery -->
 <script src="js/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/bootstrap.min.js" ></script>
 
 <title><?php echo $title ?></title>
