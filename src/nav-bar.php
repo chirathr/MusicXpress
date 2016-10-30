3201b3832109a69285efed9a4a06e3337fd04fdd
Chirath02 <chirath.02@gmail.com>
2016-10-30 13:08:43 +0530

    Added navbar and sidebar

---
+++ src/nav-bar.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chirath
+ * Date: 30/10/16
+ * Time: 1:05 PM
+ */
+
---
+++ src/sidebar.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chirath
+ * Date: 30/10/16
+ * Time: 1:05 PM
+ */
