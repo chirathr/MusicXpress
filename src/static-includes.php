a8618760c9c9987f8469eba71b131cca8af6c208
Chirath02 <chirath.02@gmail.com>
2016-10-30 13:15:38 +0530

    Added static includes.php

---
+++ src/static-includes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chirath
+ * Date: 30/10/16
+ * Time: 1:10 PM
+ */
+?>
+<link rel="stylesheet" href="../css/bootstrap.min.css" >
+<!-- Optional theme -->
+<link rel="stylesheet" href="../css/bootstrap-theme.min.css" >
+<!-- Latest compiled and minified JavaScript -->
+<script src="../js/bootstrap.min.js" ></script>
+
+<title><?php echo $title ?></title>
