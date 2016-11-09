c643b2696816cb815b236895aace1572acdb9f3f
Chirath02 <chirath.02@gmail.com>
2016-11-09 21:08:32 +0530

    fixed image issue in playlist

--- playlist/list-playlist.php
+++ playlist/list-playlist.php
@@ -9,12 +9,12 @@
 <div class="col-sm-2 col-xs-4">
     <?php
         $query = "select songId from playlistSongs where playlistid = " . $playlist[0] . ";";
-        $song = pg_fetch_row(pg_query($query))[0];
-        $query = "select * from songs where id = " . $songId[2] . ";";
+        $songId = pg_fetch_row(pg_query($query))[0];
+        $query = "select * from songs where id = " . $songId . ";";
         $song = pg_fetch_row(pg_query($query));
     ?>
     <div id="tile3" class="tile">
-        <img src="<?php if($song[3])echo $song[3]; else echo 'img/default.jpg'; ?>" class="img-responsive"/>
+        <img src="<?php if($song[3]) echo $song[3]; else echo 'img/default.jpg'; ?>" class="img-responsive"/>
         <p style="text-align:center;"><?php echo $playlist[1]; ?></p>
     </div>
 </div>
