0fb073dfcfd466e04697c81007dd440f568ccb16
Chirath02 <chirath.02@gmail.com>
2016-11-22 19:42:24 +0530

    removed old css

--- src/nav-bar.php
+++ src/nav-bar.php
@@ -7,55 +7,64 @@
  */
 
 ?>
-<!---->
-<!--<nav class="navbar navbar-inverse navbar-fixed-top">-->
-<!--    <div class="container-fluid">-->
-<!--        -->
-<!--        <div class="navbar-header">-->
-<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">-->
-<!--                <span class="sr-only">Toggle navigation</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
-<!--            <a class="navbar-brand" href="index.php">Music Express</a>-->
-<!--        </div>-->
-<!---->
-<!--         Collect the nav links, forms, and other content for toggling-->
-<!--        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
-<!--            <ul class="nav navbar-nav">-->
-<!--                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>-->
-<!--                <li><a href="#">Link</a></li>-->
-<!--            </ul>-->
-<!--            <form class="navbar-form navbar-left">-->
-<!--                <div class="form-group">-->
-<!--                    <input type="text" class="form-control" placeholder="Search">-->
-<!--                </div>-->
-<!--                <button type="submit" class="btn btn-default">Submit</button>-->
-<!--            </form>-->
-<!--            <ul class="nav navbar-nav navbar-right">-->
-<!--                --><?php
-//		$username = $_SESSION['username'];
-//                if(isset($username) && $username!="") {
-//                    echo '<li><a href="#">' . ucfirst($username) . '</a></li>';
-//		    echo '<li><a href="index.php?page=music">' . ucfirst("Upload Music") . '</a></li>';
-//		    echo '<li><a href="./login.php?logout=set">' . ucfirst("Log out") . '</a></li>';
-//                }
-//                else {
-//                    echo '
-//                        <li><a href="login.php">Login</a></li>
-//                        <li><a href="register.php">Signup</a></li>';
-//                }
-//                ?>
-<!---->
-<!--            </ul>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</nav>-->
 
-<div class="nav-bar">
-    <div class="top-left-corner">
-
+<header class="bg-white-only header header-md navbar navbar-fixed-top-xs">
+    <div class="navbar-header aside bg-info nav-xs"><a class="btn btn-link visible-xs"
+                                                       data-toggle="class:nav-off-screen,open"
+                                                       data-target="#nav,html"> <i class="icon-list"></i> </a> <a
+            href="index.html" class="navbar-brand text-lt"> <i class="icon-earphones"></i> <img
+                src="images/logo.png" alt="." class="hide"> <span class="hidden-nav-xs m-l-sm">Musik</span> </a> <a
+            class="btn btn-link visible-xs" data-toggle="dropdown" data-target=".user"> <i
+                class="icon-settings"></i> </a></div>
+    <ul class="nav navbar-nav hidden-xs">
+        <li><a href="index.html#nav,.navbar-header" data-toggle="class:nav-xs,nav-xs" class="text-muted"> <i
+                    class="fa fa-indent text"></i> <i class="fa fa-dedent text-active"></i> </a></li>
+    </ul>
+    <form class="navbar-form navbar-left input-s-lg m-t m-l-n-xs hidden-xs" role="search">
+        <div class="form-group">
+            <div class="input-group"><span class="input-group-btn"> <button type="submit"
+                                                                            class="btn btn-sm bg-white btn-icon rounded"><i
+                            class="fa fa-search"></i></button> </span> <input type="text"
+                                                                              class="form-control input-sm no-border rounded"
+                                                                              placeholder="Search songs, albums..."></div>
+        </div>
+    </form>
+    <div class="navbar-right ">
+        <ul class="nav navbar-nav m-n hidden-xs nav-user user">
+            <li class="hidden-xs"><a href="index.html#" class="dropdown-toggle lt" data-toggle="dropdown"> <i
+                        class="icon-bell"></i> <span class="badge badge-sm up bg-danger count">2</span> </a>
+                <section class="dropdown-menu aside-xl animated fadeInUp">
+                    <section class="panel bg-white">
+                        <div class="panel-heading b-light bg-light"><strong>You have <span class="count">2</span>
+                                notifications</strong></div>
+                        <div class="list-group list-group-alt"><a href="index.html#" class="media list-group-item">
+                                <span class="pull-left thumb-sm"> <img src="images/a0.png" alt="..." class="img-circle"> </span>
+                                <span class="media-body block m-b-none"> Use awesome animate.css<br> <small
+                                        class="text-muted">10 minutes ago</small> </span> </a> <a href="index.html#"
+                                                                                                  class="media list-group-item">
+                                <span class="media-body block m-b-none"> 1.0 initial released<br> <small
+                                        class="text-muted">1 hour ago</small> </span> </a></div>
+                        <div class="panel-footer text-sm"><a href="index.html#" class="pull-right"><i
+                                    class="fa fa-cog"></i></a> <a href="index.html#notes"
+                                                                  data-toggle="class:show animated fadeInRight">See all
+                                the notifications</a></div>
+                    </section>
+                </section>
+            </li>
+            <li class="dropdown"><a href="index.html#" class="dropdown-toggle bg clear" data-toggle="dropdown">
+                    <span class="thumb-sm avatar pull-right m-t-n-sm m-b-n-sm m-l-sm"> <img src="images/a0.png"
+                                                                                            alt="..."> </span>
+                    John.Smith <b class="caret"></b> </a>
+                <ul class="dropdown-menu animated fadeInRight">
+                    <li><span class="arrow top"></span> <a href="index.html#">Settings</a></li>
+                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="index.html#"> <span class="badge bg-danger pull-right">3</span> Notifications </a>
+                    </li>
+                    <li><a href="docs.html">Help</a></li>
+                    <li class="divider"></li>
+                    <li><a href="modal.lockme.html" data-toggle="ajaxModal">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
     </div>
-
-</div>
+</header>
--- src/static-includes.php
+++ src/static-includes.php
@@ -12,6 +12,18 @@
 <!-- custom css -->
 <link rel="stylesheet" href="css/styles.css">
 
+<meta charset="utf-8"/>
+<title>Musik | Web Application</title>
+<meta name="description"
+      content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+<link rel="stylesheet" href="js/jPlayer/jplayer.flat.css" type="text/css"/>
+<link rel="stylesheet" href="css/app.v1.css" type="text/css"/>
+<!--[if lt IE 9]>
+<script src="js/ie/html5shiv.js"></script>
+<script src="js/ie/respond.min.js"></script>
+<script src="js/ie/excanvas.js"></script> <![endif]-->
+
 <!-- jQuery -->
 <script src="js/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
